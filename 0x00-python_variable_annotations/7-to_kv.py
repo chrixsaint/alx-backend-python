ff62098a5aa2d5739f3636a133646212a862484f
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-06-07 10:12:32 +0100

    all

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" Complex types - string and int/float to tuple"""
-from typing import  Union, Tuple
+"""Contains a function that converts a Python variable to a KV pair."""
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """ returns arguments as a tuple """
-    return (k, float(v ** 2))
+    """Converts a Python variable to a KV pair."""
+    return k, v ** 2
